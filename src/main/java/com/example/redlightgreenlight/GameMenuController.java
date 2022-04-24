5c20da954401038d3561db7ee8dc0c6a83a9b20e
sevon <savannah.evonko@gmail.com>
2022-04-24 14:25:11 -0400

    Game Menu try

--- src/main/java/com/example/redlightgreenlight/GameMenuController.java
+++ src/main/java/com/example/redlightgreenlight/GameMenuController.java
@@ -1,10 +1,10 @@
 package com.example.redlightgreenlight;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
@@ -12,49 +12,72 @@ import java.io.IOException;
 
 public class GameMenuController {
 
-        @FXML
-        private Text Exit;
-
-        @FXML
-        private Rectangle ExitRectangle;
-
-        @FXML
-        private Text HowToPlay;
-
-        @FXML
-        private Rectangle HowToPlayRectangle;
-
-        @FXML
-        private Rectangle InstructionsRectangle;
-
-        @FXML
-        private Text InstructionsTextArea;
-
-        @FXML
-        private Text Play;
-
-        @FXML
-        private Rectangle PlayRectangle;
+    @FXML
+    private Text Exit;
 
 
-        @FXML
-        private Pane ScreenPane;
-
-        @FXML
-        void OnMouseClicked(MouseEvent event) throws IOException {
-
-        }
-
-        @FXML
-        void OnMouseEntered(MouseEvent event) {
+    @FXML
+    private Text HowToPlay;
 
 
-        }
+    @FXML
+    private Rectangle InstructionsRectangle;
 
-        @FXML
-        void OnMouseExited(MouseEvent event) {
+    @FXML
+    private Text InstructionsTextArea;
 
-        }
+    @FXML
+    private Text Play;
+
+
+    @FXML
+    void exitButtonPressed(ActionEvent event) {
 
     }
 
+    @FXML
+    void exitMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exitMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void howToPlayMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void howToPlayMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void howToPlayPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playButtonPressed(ActionEvent event) throws IOException {
+        Redlightgreenlight game = new Redlightgreenlight();
+        game.changeScene("Level1.fxml");
+
+    }
+
+    @FXML
+    void playMouseEntered(MouseEvent event) {
+        Play.setFill(Color.GREEN);
+        System.out.println("Hello");
+
+    }
+
+    @FXML
+    void playMouseExited(MouseEvent event) {
+        Play.setFill(Color.DEEPPINK);
+
+    }
+
+}
